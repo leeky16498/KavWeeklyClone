7bbaab423fb599cfdcf445ad489168209809d05e
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-14 20:13:48 +0000

    Initial Commit

---
+++ KavWeeklyClone/KavWeeklyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  KavWeeklyCloneApp.swift
+//  KavWeeklyClone
+//
+//  Created by Kyungyun Lee on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct KavWeeklyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
